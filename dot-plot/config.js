dd937ad2c199eeb14f8a1f9346f9bb9afcce7821
henryjameslau <henry.james.lau@gmail.com>
2022-10-19 21:36:45 +0100

    dot plot and grouped bar chart

---
+++ dot-plot/config.js
@@ -0,0 +1,45 @@
+config={
+  "essential": {
+    "graphic_data_url": "data.csv",
+    "legendLabels": ["2015-2019", "2020"],
+    "colour_palette": ["#6D6E72", "#0075A3"],
+    "sourceText": "Office for National Statistics",
+    "accessibleSummary":"Chart showing death rates due to alchohol by regions, for males, females and all.",
+    "xDomain":"auto"
+    // either auto or a custom domain as an array e.g [0,100]
+  },
+  "optional": {
+    "margin": {
+      "sm": {
+        "top": 15,
+        "right": 20,
+        "bottom": 20,
+        "left": 100
+      },
+      "md": {
+        "top": 15,
+        "right": 20,
+        "bottom": 20,
+        "left": 100
+      },
+      "lg": {
+        "top": 15,
+        "right": 20,
+        "bottom": 20,
+        "left": 100
+      }
+    },
+    "seriesHeight":{
+      "sm":40,
+      "md":40,
+      "lg":40
+    },
+    "xAxisTicks":{
+      "sm":4,
+      "md":8,
+      "lg":10
+    },
+    "mobileBreakpoint": 510,
+    "mediumBreakpoint": 600
+  }
+};
