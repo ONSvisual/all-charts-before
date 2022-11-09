18f6c4f7362b4e732191d5ff757f8db40b8fd3d4
henryjameslau <henry.james.lau@gmail.com>
2022-11-09 22:12:58 +0000

    got it good
    Need to sort out text labels and colours. don't need auto adjustment as numbers aren't going to clash?

---
+++ bump-chart-2nd-go/config.js
@@ -0,0 +1,50 @@
+config={
+  "essential": {
+    "graphic_data_url": "data.csv",
+    "colour_palette": {
+
+
+      type:"custom",
+      palette:["#206095","#F66068","#C6C6C6","#A8BD3A","#27A0CC"],
+
+      // type:"sequential",
+      // palette:"YlGnBu"
+      // options include custom, sequential or qualitative
+      // for custom, this is a colour for each series
+      // for sequential, this accepts a colour brewer palette eg. YlGnBu
+      // for qualitative, this accepts a colour brewer palette e.g Accent
+    },
+    "sourceText": "Office for National Statistics",
+    "accessibleSummary":"Languages spoken between two time points.",
+    "customRankCutoff":9
+  },
+  "optional": {
+    "seriesHeight":{
+      "sm":30,
+      "md":30,
+      "lg":20
+    },
+    "margin": {
+      "sm": {
+        "top": 15,
+        "right": 150,
+        "bottom": 20,
+        "left": 100
+      },
+      "md": {
+        "top": 50,
+        "right": 150,
+        "bottom": 50,
+        "left": 100
+      },
+      "lg": {
+        "top": 50,
+        "right": 125,
+        "bottom": 50,
+        "left": 35
+      }
+    },
+    "mobileBreakpoint": 510,
+    "mediumBreakpoint": 600
+  }
+};
