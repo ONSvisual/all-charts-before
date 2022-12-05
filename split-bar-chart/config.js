a83a4ee98566bd37692da2c7a633186275497f63
henryjameslau <henry.james.lau@gmail.com>
2022-12-05 20:46:18 +0000

    Think I'm done

--- split-bar-chart/config.js
+++ split-bar-chart/config.js
@@ -1,12 +1,9 @@
 config = {
   graphic_data_url:"data.csv",
-  legendLabels: ['red', 'blue'],
-  colour_palette: ["Red", 'Blue'],
+  colour_palette: "#206095",
   numberFormat:".0f",
   rowWidth:"140",
   // rowWidth set the width of y category column in pixel
   accessibleSummary:"This chart has been hidden from screen readers. The main message of the chart is summarised in the chart title.",
-  mediumBreakpoint:550,
-  mobileBreakpoint:480,
   sourceText:"Office for National Statistics – Census 2021"
 };
--- split-bar-chart/script.js
+++ split-bar-chart/script.js
@@ -10,18 +10,6 @@ function drawGraphic() {
   //population accessible summmary
   d3.select('#accessibleSummary').html(config.accessibleSummary)
 
-  var threshold_md = config.mediumBreakpoint;
-  var threshold_sm = config.mobileBreakpoint;
-
-  //set variables for chart dimensions dependent on width of #graphic
-  if (parseInt(graphic.style("width")) < threshold_sm) {
-    size = "sm"
-  } else if (parseInt(graphic.style("width")) < threshold_md) {
-    size = "md"
-  } else {
-    size = "lg"
-  }
-
   formatNo = d3.format(config.numberFormat)
 
   // set up scale
@@ -51,24 +39,18 @@ function drawGraphic() {
   // create a div for the headers
   headers = chart.append('div').attr('class', 'splitBar-label')
 
-
   // create div for the first square
   headers.append('div')
   .attr('class', 'rowLabel')
-  .style('display','inline-block')
   .style('width',config.rowWidth+'px')
 
   // create divs for the rest of the column headers
-  headers.append('div')
-  .style('margin-right','-8px')
+  headers.append('div').attr('class','headers')
   .style('width',`calc(100% - ${config.rowWidth}px)`)
-  .style('display','inline-block')
   .selectAll('div.column').data(xcategories)
   .join('div')
   .attr('class', 'column')
   .style('width',100/xcategories.length+'%')
-  .style('padding-right','8px')
-  .style('display','inline-block')
   .append('span').html(d=>d)
 
   // create divs as rows 
@@ -80,31 +62,27 @@ function drawGraphic() {
   // first div as separate 
   rows.append('div').attr('class','rowLabel')
   .style('width',config.rowWidth+'px')
-  .style('display','inline-block')
   .append('span')
   .style('text-align','right')
   .html(d=>d[0])
 
   // then create another div to hold all split bars
-  splitBar=rows.append('div').attr('class','splitBars')
+  splitBar=rows.append('div').attr('class','headers')
   .style('width',`calc(100% - ${config.rowWidth}px)`)
-  .style('display','inline-block')
-  .style('margin-right','-8px')
   .selectAll('div.splitBar')
   .data(d=>d[1])
   .join('div')
   // then add a div for each x category
-  .attr('class','splitBar')
+  .attr('class','column')
   .style('width',100/xcategories.length+'%')
-  .style('padding-right','8px')
 
   // divs for inside the splitBar
   splitBarInner=splitBar.append('div').attr('class','splitBar-inner')
   // then div for the background
   .append('div')
   .attr('class','splitBar-inner--background')
-  .style('position','relative')
 
+  // add a div to help draw a line for 0
   splitBarInner.append('div')
   .attr('class','splitBar-bar--value')
   .style('left',0)
@@ -118,28 +96,29 @@ function drawGraphic() {
   .attr('class','splitBar-bar--value')
   .style('left',d=>+d.value>0 ? x(0)+"%" : x(+d.value)+"%")
   .style('right',d=>+d.value>0 ? 100-x(+d.value)+"%" : (100-x(0))+"%")
-  .style('background','#206095')
+  .style('background',config.colour_palette)
   .append('div')
   // then a div to hold the value
   .attr('class','splitBar-bar--label')
-  .style('text-align','left')
-  .style('padding','0 5px')
-  .style('margin-left',d=>Math.abs(+x(d.value)-x(0))<20 ? "100%" : "calc(100% - 35px)")
-  .style('line-height','27px')
+  .style('margin-left',(d)=>{
+    if(d.value>0){
+      return Math.abs(+x(d.value)-x(0))<20 ? "100%" : "calc(100% - 35px)"
+    }else{
+      return Math.abs(+x(d.value)-x(0))>20 ? "0%" : "calc(0% - 35px)"
+    }
+  })
   .append('span')
   .style('color',d=>Math.abs(+x(d.value)-x(0))<20 ? "#222222": "#fff")
-
   .html(d=>formatNo(d.value))
 
   // final div for the zero indicator
-  finalrow=chart.append('div')
+  finalrow=chart.append('div').attr('class','finalRow')
   // first div as separate
   finalrow.append('div').attr('class','rowLabel')
   .style('width',config.rowWidth+'px')
-  .style('display','inline-block')
 
-  finalrow.append('div')
-  .style('margin-right','-8px')
+  finalrow.append('div').attr('class','')
+  .style('margin-right','-10px')
   .style('width',`calc(100% - ${config.rowWidth}px)`)
   .style('display','inline-block')
   .selectAll('div.column').data(xcategories)
@@ -150,29 +129,8 @@ function drawGraphic() {
   .style('display','inline-block')
   .append('span')
   .style('position','relative')
-  .style('left',x(0)+"%")
+  .style('left','calc('+x(0)+"%"+' - 5px)')
   .html(0)
-
-
-
-
-  // Set up the legend
-  // var legenditem = d3.select('#legend')
-  //   .selectAll('div.legend--item')
-  //   .data(d3.zip(Object.values(config.legendLabels), config.colour_palette))
-  //   .enter()
-  //   .append('div')
-  //   .attr('class', 'legend--item')
-
-  // legenditem.append('div').attr('class', 'legend--icon')
-  //   .style('background-color', function(d) {
-  //     return d[1]
-  //   })
-
-  // legenditem.append('div')
-  //   .append('p').attr('class', 'legend--text').html(function(d) {
-  //     return d[0]
-  //   })
 
   //create link to source
   d3.select("#source")
