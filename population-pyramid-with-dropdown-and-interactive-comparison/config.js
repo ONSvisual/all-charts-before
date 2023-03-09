729b3234d7a564e886295c8bf07ed5f4a6f12993
Tim Kershaw <ctkershaw@gmail.com>
2023-03-09 11:41:11 +0000

    Update config.js

--- population-pyramid-with-dropdown-and-interactive-comparison/config.js
+++ population-pyramid-with-dropdown-and-interactive-comparison/config.js
@@ -6,11 +6,11 @@ config = {
     comparison_data: "comparison.csv",
     comparison_time_data: "comparison-time.csv",
     dataType: "numbers",
-    // dataType can be a 'percentage' or 'numbers' where it works out the percentage in the script
+    // dataType can be a "percentage" or "numbers" where it works out the percentage in the script
     colour_palette: ["#9A86E9", "#3fb0b3"],
     // this is the lighter palette for reference lines ["#9A86E9", "#3fb0b3"]
     comparison_colour_palette: ["#5c5185", '#306970'],
-    legend: ['Selected area','2011'],
+    legend: ["Selected area","2011"],
     xAxislabel: ["Percentage"]
 
 
