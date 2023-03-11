2b44ffd5f98478fd2e6d2d4a015a175dc612e6cd
Tim Kershaw <ctkershaw@gmail.com>
2023-03-11 12:38:46 +0000

    Update config.js

--- static-population-pyramid-with-comparison/config.js
+++ static-population-pyramid-with-comparison/config.js
@@ -1,16 +1,16 @@
 config = {
-  essential: {
-    accessibleSummary: "This chart has been hidden from screen readers. The main message of the chart is summarised in the chart title.",
-    sourceText: "Office for National Statistics – Census 2021",
-    graphic_data_url: "data.csv",
-    comparison_data: "comparison.csv",
-    dataType: "numbers",
+  "essential: {
+    "accessibleSummary": "This chart has been hidden from screen readers. The main message of the chart is summarised in the chart title.",
+    "sourceText": "Office for National Statistics – Census 2021",
+    "graphic_data_url": "data.csv",
+    "comparison_data": "comparison.csv",
+    "dataType": "numbers",
     // dataType can be a 'percentage' or 'numbers' where it works out the percentage in the script
-    colour_palette: ["#9A86E9", "#3fb0b3"],
+    "colour_palette": ["#9A86E9", "#3fb0b3"],
     // this is the lighter palette for reference lines ["#9A86E9", "#3fb0b3"]
-    comparison_colour_palette: ["#5c5185", '#306970'],
-    legend: ["Area name", 'England and Wales'],
-    xAxislabel: ["Percentage"]
+    "comparison_colour_palette": ["#5c5185", '#306970'],
+    "legend": ["Area name", 'England and Wales'],
+    "xAxislabel": ["Percentage"]
 
   },
   "optional": {
