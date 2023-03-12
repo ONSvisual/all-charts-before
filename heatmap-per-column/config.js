c8ce8ba1f32b2d39f349a0b5ef7f7acf5333dd23
Tim Kershaw <tim.kershaw@ons.gov.uk>
2023-03-12 10:50:31 +0000

    removed stray comma

--- heatmap-per-column/config.js
+++ heatmap-per-column/config.js
@@ -41,5 +41,5 @@ config={
     "mobileBreakpoint": 510,
     "mediumBreakpoint": 600
   },
-  "elements":{"select":0, "nav":0, "legend":0, "titles":0},
+  "elements":{"select":0, "nav":0, "legend":0, "titles":0}
 };
